115ee3532819fbff2c4ce19127d2c7f66adb633a
jD91mZM2 <me@krake.one>
2018-05-18 20:45:07 +0200

    Make TcpListener sort of work on redox

--- tokio-tcp/src/listener.rs
+++ tokio-tcp/src/listener.rs
@@ -178,7 +178,10 @@ impl TcpListener {
     /// # }
     /// ```
     pub fn poll_accept_std(&mut self) -> Poll<(net::TcpStream, SocketAddr), io::Error> {
+        #[cfg(not(target_os = "redox"))]
         try_ready!(self.io.poll_read_ready(mio::Ready::readable()));
+        #[cfg(target_os = "redox")]
+        try_ready!(self.io.poll_write_ready());
 
         match self.io.get_ref().accept_std() {
             Ok(pair) => Ok(pair.into()),
